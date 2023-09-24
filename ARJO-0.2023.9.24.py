9102a85e89beaa18e9369b69dd920612b9e5e5d1
Mr. RDEV <122167559+mr-rdev@users.noreply.github.com>
2023-09-24 10:32:54 -0400

    Update and rename ARJO-0.2023.9.23.2.py to ARJO-0.2023.9.24.py
    - Changed ARJO install reference to a list rather than a dictionary
    - Installed the Registry Info. as a Dictionary on the system rather than a list.

--- ARJO-0.2023.9.24.py
+++ ARJO-0.2023.9.24.py
@@ -5,12 +5,14 @@ from os.path import expanduser
 ARJO_REG_DATA = {
     "osName": "ARJO SHELL",
     "osCodename": "CODENAME ARJO SHELL",
-    "version": "0.2023.9.23.2",
+    "version": "0.2023.9.24",
     "lastStableVersion": "0",
     "yearCreated": "2023",
     "monthCreated": "9",
-    "dayCreated": "23.2",
+    "dayCreated": "24",
 }
+
+ARJO_REG_LIST = ["ARJO SHELL", "CODENAME ARJO SHELL", "0.2023.9.24", "0", "2023", "9", "24"]
 
 # Name of the registry filename
 REG_FILENAME = "ARJO_REG.arjo"
@@ -22,8 +24,8 @@ print(f"SETUP FOR: {ARJO_REG_DATA['version']}")
 #Registry Setup
 try:
     with open(f"{expanduser('~')}\{REG_FILENAME}", 'w') as ARJO_REGISTRY:
-        for i in ARJO_REG_DATA:
-            ARJO_REGISTRY.write(ARJO_REG_DATA[i])
+        for i in ARJO_REG_LIST:
+            ARJO_REGISTRY.write(i)
             ARJO_REGISTRY.write("\n")
     print("\nREGISTRY HAS COMPLETED SUCCESSFULLY")
 except Exception as error:
@@ -36,24 +38,15 @@ try:
     registry_bind = []
     with open(f"{expanduser('~')}\{REG_FILENAME}", 'r') as ARJO_REGISTRY:
         with open("ARJO.py", 'w') as ARJO:
-            for i in ARJO_REGISTRY:
-                i = list(i)
-                del i[-1]
-                j = ""
-                for item in i:
-                    for subitem in item:
-                        j += subitem
-                i = j
-                registry_bind.append(i)
-            ARJO.write("RAW_REGISTRY = [")
-            for i in registry_bind:
-                if i != registry_bind[-1]:
-                    ARJO.write(f"'{i}'")
-                    ARJO.write(",")
+            ARJO.write("ARJO_REG_DATA = {\n")
+            for i in ARJO_REG_DATA:
+                ARJO_REG_DATA_I = ARJO_REG_DATA[i]
+                if ARJO_REG_DATA[i] != ARJO_REG_LIST[-1]:
+                    ARJO.write(f"   '{i}': '{ARJO_REG_DATA_I}',\n")    
                 else:
-                    ARJO.write(f"'{i}'")
-            ARJO.write("]")
-            ARJO.write("\nprint(RAW_REGISTRY)")
+                    ARJO.write(f"   '{i}': '{ARJO_REG_DATA_I}'\n")
+            ARJO.write("}\n")
+            ARJO.write("print(ARJO_REG_DATA)")
             print("REGISTRY BINDING HAS COMPLETED SUCCESSFULLY")
 except Exception as error:
     print("FATAL ERROR - REGISTRY BINDING ERROR (0x002)")
