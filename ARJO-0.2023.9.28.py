f081d2dd6a5486a86a6fd2364801eb4b58b30fa0
Mr. RDEV <122167559+mr-rdev@users.noreply.github.com>
2023-09-27 19:20:25 -0400

    Update and rename ARJO-0.2023.9.27.2.py to ARJO-0.2023.9.28.py

--- ARJO-0.2023.9.28.py
+++ ARJO-0.2023.9.28.py
@@ -4,11 +4,11 @@ import os
 ARJO_REG_DATA = {
     "osName": "ARJO SHELL",
     "osCodename": "APPLESEED",
-    "version": "0.2023.9.27.2",
+    "version": "0.2023.9.28",
     "lastStableVersion": "0",
     "yearCreated": "2023",
     "monthCreated": "9",
-    "dayCreated": "27.2",
+    "dayCreated": "28",
 }
 
 #Creating the File
@@ -86,18 +86,19 @@ try:
         ARJO.write("                if filter in ARJO_REG_DATA:\n")
         ARJO.write("                    print(ARJO_REG_DATA[filter])\n")
         ARJO.write("            elif subcommand == 'save':\n")
-        ARJO.write(f"                os.remove('{REG_FILENAME}'\n")
+        ARJO.write(f"                os.remove('{REG_FILENAME}')\n")
         ARJO.write(f"                with open('{REG_FILENAME}', 'w') as EXTERNAL_REGISTRY:\n")
         ARJO.write(f"                    for i in ARJO_REG_DATA:\n")
-        ARJO.write(f"                        EXTERNAL_REGISTRY.write(i)\n")
-        ARJO.write(f"                    if os.name == 'nt':\n"
-        ARJO.write(f"                        os.system(f'attrib +h {REG_FILENAME}')\n"
+        ARJO.write(f"                        EXTERNAL_REGISTRY.write(ARJO_REG_DATA[i])\n")
+        ARJO.write(f"                    if os.name == 'nt':\n")
+        ARJO.write(f"                        os.system(f'attrib +h {REG_FILENAME}')\n")
         ARJO.write("            elif subcommand == 'edit':\n")
         ARJO.write("                filter = input('Insert Registry Object to Edit: ')\n")
         ARJO.write("                if filter in ARJO_REG_DATA:\n")
         ARJO.write("                    for i in ARJO_REG_DATA:\n")
         ARJO.write("                        if i == filter:\n")
         ARJO.write("                            ARJO_REG_DATA[i] = input('Insert New Data: ')\n")
+        ARJO.write("                            ARJO_REG_DATA[i] += '\\n'")
 except Exception as error:
     print("\nUNKNOWN ERROR")
     print("INFORMATION:\n", error)
